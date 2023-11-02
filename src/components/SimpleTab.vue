f4c70124834a84f91312c4f3df49da383b5b6100
Philipp Oeschger <kontakt@philipp-oeschger.de>
2023-11-02 22:32:35 +0100

    remove console.logs

--- src/components/SimpleTab.vue
+++ src/components/SimpleTab.vue
@@ -1,26 +1,26 @@
 <template>
-    <div :class="getClasses" v-if="isVisible">
-        <slot></slot>
-    </div>
+  <div :class="getClasses" v-if="isVisible">
+    <slot></slot>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { useSimpleTabsStore } from '@/composable/tabStore';
+import { computed } from "vue";
+import { useSimpleTabsStore } from "@/composable/tabStore";
 
 interface TabProps {
-    /**
-     * The tab group, this tab belongs to.
-     */
-    tabGroup: string;
-    /**
-     * The Tab Identifier/s.
-     */
-    tabName: string | string[];
-    /**
-     * Custom CSS classes.
-     */
-    customClasses?: string[];
+  /**
+   * The tab group, this tab belongs to.
+   */
+  tabGroup: string;
+  /**
+   * The Tab Identifier/s.
+   */
+  tabName: string | string[];
+  /**
+   * Custom CSS classes.
+   */
+  customClasses?: string[];
 }
 
 const props = defineProps<TabProps>();
@@ -29,33 +29,30 @@ const tabStore = useSimpleTabsStore();
 
 let tabs: Array<string>;
 
-console.log('is entry');
 if (props.tabName instanceof Array) {
-    console.log('is array');
-    tabStore.registerTab(props.tabGroup, props.tabName);
-    tabs = props.tabName;
-} else if (typeof props.tabName === 'string') {
-    console.log('is string');
-    tabStore.registerTab(props.tabGroup, [<string>props.tabName]);
-    tabs = [<string>props.tabName];
+  tabStore.registerTab(props.tabGroup, props.tabName);
+  tabs = props.tabName;
+} else if (typeof props.tabName === "string") {
+  tabStore.registerTab(props.tabGroup, [<string>props.tabName]);
+  tabs = [<string>props.tabName];
 }
 
 const isVisible = computed(() => {
-    const selectedTab = tabStore.getOpenTab(props.tabGroup);
-    if (selectedTab !== undefined) {
-        const tabSelected = selectedTab.some((val) => {
-            return tabs.find((tab) => tab === val);
-        });
-        return tabSelected;
-    }
-    return false;
+  const selectedTab = tabStore.getOpenTab(props.tabGroup);
+  if (selectedTab !== undefined) {
+    const tabSelected = selectedTab.some((val) => {
+      return tabs.find((tab) => tab === val);
+    });
+    return tabSelected;
+  }
+  return false;
 });
 
 const getClasses = computed(() => {
-    const array = ['simpleT-tab'];
-    if (props.customClasses !== undefined) {
-        array.push(...props.customClasses);
-    }
-    return array.join(' ');
+  const array = ["simpleT-tab"];
+  if (props.customClasses !== undefined) {
+    array.push(...props.customClasses);
+  }
+  return array.join(" ");
 });
 </script>
